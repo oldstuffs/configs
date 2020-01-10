57a494c96f56dd0c8faba038b2925fc732f7248b
portlek <hsndmrts_@outlook.com>
2020-01-10 13:59:09 +0300

    Update MyPlugin.java

--- src/test/java/io/github/portlek/configs/MyPlugin.java
+++ src/test/java/io/github/portlek/configs/MyPlugin.java
@@ -1,13 +1,11 @@
 package io.github.portlek.configs;
 
 import io.github.portlek.configs.file.Config;
-import io.github.portlek.configs.file.Messages;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.io.File;
-import java.util.Locale;
 
 public final class MyPlugin extends JavaPlugin {
 
@@ -25,15 +23,6 @@ public final class MyPlugin extends JavaPlugin {
     public void onEnable() {
         final Config config = annotationProcessor.load(
             new Config()
-        );
-        final Messages messages = annotationProcessor.load(
-            new Messages(
-                new Locale(config.plugin_language)
-            )
-        );
-
-        System.out.println(
-            messages.error.player_not_found
         );
     }
 
