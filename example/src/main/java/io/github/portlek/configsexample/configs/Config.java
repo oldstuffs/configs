bfe7485a8c5c8ed7576e0a07571c876c96cd954c
portlek <hsndmrts_@outlook.com>
2020-01-10 19:09:03 +0300

    copyright update

--- example/src/main/java/io/github/portlek/configsexample/configs/Config.java
+++ example/src/main/java/io/github/portlek/configsexample/configs/Config.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019 portlek
+ * Copyright (c) 2019 Hasan Demirtaş
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@
  *
  */
 
-package io.github.portlek.example.configs;
+package io.github.portlek.configsexample.configs;
 
 import io.github.portlek.configs.annotations.File;
 import io.github.portlek.configs.annotations.Value;
