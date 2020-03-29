cbfd4c1fb965b6bfe199df1bd049da308f52db5b
Hasan Demirtaş <utsukushihito@outlook.com>
2020-03-29 16:30:47 +0300

    Update Provided.java

--- core/src/main/java/io/github/portlek/configs/Provided.java
+++ core/src/main/java/io/github/portlek/configs/Provided.java
@@ -5,7 +5,16 @@ import org.jetbrains.annotations.NotNull;
 
 public interface Provided<T> {
 
+    default void set(@NotNull final T t, @NotNull final ConfigSection section) {
+        this.set(t, section, "");
+    }
+
     void set(@NotNull T t, @NotNull ConfigSection section, @NotNull String path);
+
+    @NotNull
+    default Optional<T> getWithField(@NotNull final T t, @NotNull final ConfigSection section) {
+        return this.getWithField(t, section, "");
+    }
 
     @NotNull
     default Optional<T> getWithField(@NotNull final T t, @NotNull final ConfigSection section,
@@ -15,5 +24,10 @@ public interface Provided<T> {
 
     @NotNull
     Optional<T> get(@NotNull ConfigSection section, @NotNull String path);
+
+    @NotNull
+    default Optional<T> get(@NotNull final ConfigSection section) {
+        return this.get(section, "");
+    }
 
 }
