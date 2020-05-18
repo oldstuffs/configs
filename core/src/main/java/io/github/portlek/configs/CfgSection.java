378b9e063040ed53022085f79b5f755883bd54b9
Hasan Demirtaş <utsukushihito@outlook.com>
2020-05-19 02:44:28 +0300

    Update CfgSection.java

--- core/src/main/java/io/github/portlek/configs/CfgSection.java
+++ core/src/main/java/io/github/portlek/configs/CfgSection.java
@@ -5,6 +5,7 @@ import io.github.portlek.configs.util.Feature;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -87,6 +88,24 @@ public interface CfgSection {
     void setup(@NotNull FlManaged managed, @NotNull ConfigurationSection configurationSection);
 
     @NotNull
+    default <T> Optional<List<T>> getGenericList(@NotNull final String path,
+                                                 @NotNull final Function<String, List<T>> function) {
+        if (!this.contains(path) || !this.getList(path).isPresent()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(function.apply(path));
+    }
+
+    @NotNull
+    default <T> Optional<T> getGenericObject(@NotNull final String path,
+                                             @NotNull final Function<String, T> function) {
+        if (this.contains(path)) {
+            return Optional.ofNullable(function.apply(path));
+        }
+        return Optional.empty();
+    }
+
+    @NotNull
     default Optional<String> getString(@NotNull final String path) {
         return Optional.ofNullable(this.getConfigurationSection().getString(path));
     }
@@ -96,32 +115,32 @@ public interface CfgSection {
         return Optional.ofNullable(this.getConfigurationSection().getString(path, def));
     }
 
-    default int getInt(@NotNull final String path) {
-        return this.getConfigurationSection().getInt(path);
+    default Optional<Integer> getInt(@NotNull final String path) {
+        return this.getGenericObject(path, this.getConfigurationSection()::getInt);
     }
 
     default int getInt(@NotNull final String path, final int def) {
         return this.getConfigurationSection().getInt(path, def);
     }
 
-    default boolean getBoolean(@NotNull final String path) {
-        return this.getConfigurationSection().getBoolean(path);
+    default Optional<Boolean> getBoolean(@NotNull final String path) {
+        return this.getGenericObject(path, this.getConfigurationSection()::getBoolean);
     }
 
     default boolean getBoolean(@NotNull final String path, final boolean def) {
         return this.getConfigurationSection().getBoolean(path, def);
     }
 
-    default double getDouble(@NotNull final String path) {
-        return this.getConfigurationSection().getDouble(path);
+    default Optional<Double> getDouble(@NotNull final String path) {
+        return this.getGenericObject(path, this.getConfigurationSection()::getDouble);
     }
 
     default double getDouble(@NotNull final String path, final double def) {
         return this.getConfigurationSection().getDouble(path, def);
     }
 
-    default long getLong(@NotNull final String path) {
-        return this.getConfigurationSection().getLong(path);
+    default Optional<Long> getLong(@NotNull final String path) {
+        return this.getGenericObject(path, this.getConfigurationSection()::getLong);
     }
 
     default long getLong(@NotNull final String path, final long def) {
@@ -129,48 +148,48 @@ public interface CfgSection {
     }
 
     @NotNull
-    default List<String> getStringList(@NotNull final String path) {
-        return this.getConfigurationSection().getStringList(path);
+    default Optional<List<String>> getStringList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getStringList);
     }
 
     @NotNull
-    default List<Integer> getIntegerList(@NotNull final String path) {
-        return this.getConfigurationSection().getIntegerList(path);
+    default Optional<List<Integer>> getIntegerList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getIntegerList);
     }
 
     @NotNull
-    default List<Boolean> getBooleanList(@NotNull final String path) {
-        return this.getConfigurationSection().getBooleanList(path);
+    default Optional<List<Boolean>> getBooleanList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getBooleanList);
     }
 
     @NotNull
-    default List<Double> getDoubleList(@NotNull final String path) {
-        return this.getConfigurationSection().getDoubleList(path);
+    default Optional<List<Double>> getDoubleList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getDoubleList);
     }
 
     @NotNull
-    default List<Float> getFloatList(@NotNull final String path) {
-        return this.getConfigurationSection().getFloatList(path);
+    default Optional<List<Float>> getFloatList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getFloatList);
     }
 
     @NotNull
-    default List<Long> getLongList(@NotNull final String path) {
-        return this.getConfigurationSection().getLongList(path);
+    default Optional<List<Long>> getLongList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getLongList);
     }
 
     @NotNull
-    default List<Byte> getByteList(@NotNull final String path) {
-        return this.getConfigurationSection().getByteList(path);
+    default Optional<List<Byte>> getByteList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getByteList);
     }
 
     @NotNull
-    default List<Character> getCharacterList(@NotNull final String path) {
-        return this.getConfigurationSection().getCharacterList(path);
+    default Optional<List<Character>> getCharacterList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getCharacterList);
     }
 
     @NotNull
-    default List<Short> getShortList(@NotNull final String path) {
-        return this.getConfigurationSection().getShortList(path);
+    default Optional<List<Short>> getShortList(@NotNull final String path) {
+        return this.getGenericList(path, this.getConfigurationSection()::getShortList);
     }
 
     @NotNull
