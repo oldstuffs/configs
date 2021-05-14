e0105cad34c1ad7e071b8fb46de592a0ccc803d8
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-14 22:38:50 +0300

    checkstyle.

--- bukkit/src/main/java/io/github/portlek/configs/bukkit/loaders/FlPosition.java
+++ bukkit/src/main/java/io/github/portlek/configs/bukkit/loaders/FlPosition.java
@@ -31,7 +31,6 @@ import io.github.portlek.configs.bukkit.data.Position;
 import io.github.portlek.configs.configuration.ConfigurationSection;
 import io.github.portlek.configs.loaders.SectionFieldLoader;
 import java.util.Optional;
-import java.util.function.BiFunction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
