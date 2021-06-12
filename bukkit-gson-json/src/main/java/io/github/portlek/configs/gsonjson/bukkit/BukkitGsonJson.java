8b39f3ebafeb7f51720342fa9d28849d85d187f6
Hasan Demirtaş <utsukushihito@outlook.com>
2021-06-12 18:26:01 +0300

    move.

--- bukkit-gson-json/src/main/java/io/github/portlek/configs/gsonjson/bukkit/BukkitGsonJson.java
+++ bukkit-gson-json/src/main/java/io/github/portlek/configs/gsonjson/bukkit/BukkitGsonJson.java
@@ -25,10 +25,21 @@
 
 package io.github.portlek.configs.gsonjson.bukkit;
 
-import io.github.portlek.configs.gsonjson.GsonJson;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.github.portlek.transformer.TransformResolver;
+import io.github.portlek.transformer.declarations.FieldDeclaration;
 import io.github.portlek.transformer.declarations.GenericDeclaration;
+import io.github.portlek.transformer.declarations.TransformedObjectDeclaration;
 import io.github.portlek.transformer.exceptions.TransformException;
+import io.github.portlek.transformer.postprocessor.PostProcessor;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.configuration.ConfigurationSection;
@@ -39,7 +50,25 @@ import org.jetbrains.annotations.Nullable;
  * a class that represents json file configuration.
  */
 @RequiredArgsConstructor
-public final class BukkitGsonJson extends GsonJson {
+public final class BukkitGsonJson extends TransformResolver {
+
+  /**
+   * the gson.
+   */
+  @NotNull
+  private final Gson gson;
+
+  /**
+   * the cache map.
+   */
+  private Map<String, Object> map = new LinkedHashMap<>();
+
+  /**
+   * ctor.
+   */
+  public BukkitGsonJson() {
+    this(new GsonBuilder().setPrettyPrinting().create());
+  }
 
   @Nullable
   @Override
@@ -53,6 +82,38 @@ public final class BukkitGsonJson extends GsonJson {
     return super.deserialize(object, genericSource, targetClass, genericTarget, defaultValue);
   }
 
+  @NotNull
+  @Override
+  public List<String> getAllKeys() {
+    return List.copyOf(this.map.keySet());
+  }
+
+  @NotNull
+  @Override
+  public Optional<Object> getValue(@NotNull final String path) {
+    return Optional.ofNullable(this.map.get(path));
+  }
+
+  @Override
+  public void load(@NotNull final InputStream inputStream, @NotNull final TransformedObjectDeclaration declaration) {
+    //noinspection unchecked
+    this.map = this.gson.fromJson(PostProcessor.of(inputStream).getContext(), Map.class);
+    if (this.map == null) {
+      this.map = new LinkedHashMap<>();
+    }
+  }
+
+  @Override
+  public boolean pathExists(@NotNull final String path) {
+    return this.map.containsKey(path);
+  }
+
+  @Override
+  public void removeValue(@NotNull final String path, @Nullable final GenericDeclaration genericType,
+                          @Nullable final FieldDeclaration field) {
+    this.map.remove(path);
+  }
+
   @Nullable
   @Override
   public Object serialize(@Nullable final Object value, @Nullable final GenericDeclaration genericType,
@@ -61,6 +122,17 @@ public final class BukkitGsonJson extends GsonJson {
       return this.getMapValues((ConfigurationSection) value, false);
     }
     return super.serialize(value, genericType, conservative);
+  }
+
+  @Override
+  public void setValue(@NotNull final String path, @Nullable final Object value,
+                       @Nullable final GenericDeclaration genericType, @Nullable final FieldDeclaration field) {
+    this.map.put(path, this.serialize(value, genericType, true));
+  }
+
+  @Override
+  public void write(@NotNull final OutputStream outputStream, @NotNull final TransformedObjectDeclaration declaration) {
+    this.gson.toJson(this.map, new OutputStreamWriter(outputStream));
   }
 
   /**
