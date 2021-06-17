f677133caaf65508f45f615e50f0d17202fd706b
Hasan Demirtaş <utsukushihito@outlook.com>
2021-06-17 15:42:29 +0300

    fix stack overflow bug.

--- bukkit-hjson-json/src/main/java/io/github/portlek/configs/hjsonjson/bukkit/BukkitHJsonJson.java
+++ bukkit-hjson-json/src/main/java/io/github/portlek/configs/hjsonjson/bukkit/BukkitHJsonJson.java
@@ -103,7 +103,7 @@ public final class BukkitHJsonJson extends HJsonJson {
         final var key = entry.getKey();
         final var value = entry.getValue();
         if (value instanceof ConfigurationSection) {
-          return Map.entry(key, this.getMapValues(section, deep));
+          return Map.entry(key, this.getMapValues((ConfigurationSection) value, deep));
         }
         return Map.entry(key, value);
       })
